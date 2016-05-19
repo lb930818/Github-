20b2ac5df0cb6144f58fff1152d410944e16ac6d
破罐子 <18817951980@163.com>
2016-05-19 19:00:44 +0800

    版本一

---
+++ Github客户端测试代码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github客户端测试代码
+//
+//  Created by 转智IT教育 on 16/5/19.
+//  Copyright © 2016年 jxtv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github客户端测试代码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github客户端测试代码
+//
+//  Created by 转智IT教育 on 16/5/19.
+//  Copyright © 2016年 jxtv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
